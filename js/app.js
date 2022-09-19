e2c3432dc12f3ecdf10f153d78955fafa14a311f
Boglarka Bodnar <bodnarbog94@gmail.com>
2022-09-19 19:12:44 +0200

    up

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@ let newGameBtn = document.querySelector('.start-btn')
 let frontFace = document.querySelectorAll('.front-face')
 let firstCard = document.querySelectorAll('.one')
 let secondCard = document.querySelectorAll('.other')
-let usedId = []
-let usedOrder = []
 let flippedCards = []
 let imgs = document.getElementsByTagName("IMG")
 
@@ -13,6 +11,7 @@ for (let img of imgs) {
 }
 
 async function apiImgs() {
+    let usedId = []
     for (let i = 0; i < frontFace.length / 2; i++) {
         let random = Math.floor(Math.random() * 70) + 1
         while (usedId.includes(random)) {
@@ -29,6 +28,7 @@ async function apiImgs() {
 }
 
 function randomOrder() {
+    let usedOrder = []
     cards.forEach(card => {
         let randomNum = Math.floor(Math.random() * frontFace.length)
         while (usedOrder.includes(randomNum)) {
@@ -39,28 +39,15 @@ function randomOrder() {
     })
 }
 
-
-
-
-
-// cards.forEach(card => card.addEventListener('click', function flipCard(event) {
-//     flippedCards.push(event.target.parentNode)
-//     if (flippedCards.length === 2) {
-//         card.removeEventListener('click', flipCard)
-//     }
-//     console.log(flippedCards)
-// }))
-
 cards.forEach(card => card.addEventListener('click', flipCard))
 
-
-
 function flipCard(event) {
-    flippedCards.push(event.target.parentNode)
-    event.target.parentNode.classList.toggle('turn')
-    event.target.parentNode.classList.toggle('turned')
+    let currentCard = event.target.parentNode
+    flippedCards.push(currentCard)
+    currentCard.classList.toggle('turn')
+    currentCard.classList.toggle('turned')
     if (flippedCards.length === 1) {
-        document.querySelectorAll('.turned').forEach(card => card.removeEventListener('click', flipCard))
+        document.querySelector('.turned').removeEventListener('click', flipCard)
     }
     if (flippedCards.length === 2) {
         cards.forEach(card => card.removeEventListener('click', flipCard))
@@ -68,91 +55,30 @@ function flipCard(event) {
             flippedCards[0].classList.toggle('found')
             flippedCards[1].classList.toggle('found')
             flippedCards[0].classList.remove('turned')
-            flippedCards[1].classList.toggle('turned')
-            cards.forEach(card => card.addEventListener('click', flipCard))
+            flippedCards[1].classList.remove('turned')
+            setTimeout(function () { cards.forEach(card => card.addEventListener('click', flipCard)) }, 450)
             document.querySelectorAll('.found').forEach(card => card.removeEventListener('click', flipCard))
-            
+
         }
         else {
-            cards.forEach(card => card.addEventListener('click', flipCard))
+            setTimeout(function () { cards.forEach(card => card.addEventListener('click', flipCard)) }, 450)
             setTimeout(function () { document.querySelectorAll('.turned').forEach(card => card.classList.remove('turn', 'turned')) }, 400)
         }
-      
         flippedCards.splice(0, 2)
+    }
+    if (document.querySelectorAll('.found').length === cards.length) {
+        document.body.style.backgroundColor = 'darkgreen'
     }
 }
 
 
+function newGame() {
+    cards.forEach(card => card.classList.remove('turn', 'turned', 'found'))
+    document.body.style.removeProperty('background-color')
+    flippedCards = []
+    apiImgs()
+}
 
 
-
+newGameBtn.addEventListener('click',newGame)
 addEventListener('load', apiImgs)
-
-
-// cards.forEach(card => card.addEventListener('click', () => {
-//     flippedCards.push(card)
-//     card.classList.toggle('turn')
-//     flippedCards.forEach(flipped => flipped.style.pointerEvents = 'none')
-//     if (flippedCards.length === 2) {
-//         if (flippedCards[0].firstElementChild.src != flippedCards[1].firstElementChild.src) {
-//             setTimeout(function () {
-//                 flippedCards.forEach(flipped => flipped.classList.remove('turn'))
-//                 flippedCards.splice(0, 2)
-//             }, 400)
-//         }
-//         else {
-//             flippedCards.forEach(flipped => {
-//                 flipped.classList.toggle('available')
-//                 flipped.style.pointerEvents = 'none'
-//             })
-//             flippedCards.splice(0, 2)
-//         }
-//         let available = document.querySelectorAll('.available')
-//         available.forEach(flip => flip.style.pointerEvents = 'auto')
-//     }
-
-// }))
-
-// cards.forEach(card => card.addEventListener('click', () => {
-//     card.classList.toggle('turn')
-//     flippedCards.push(card)
-//     if (flippedCards.length === 2) {
-//         cards.forEach(flip => flip.style.pointerEvents = 'none')
-//         if (flippedCards[0].firstElementChild.src === flippedCards[1].firstElementChild.src) {
-//             for (let flippedCard of flippedCards) {
-//                 flippedCard.style.pointerEvents = 'none'
-//                 flippedCard.classList.toggle('turned')
-//                 flippedCard.classList.remove('available')
-//             }
-//             flippedCards.splice(0, 2)
-//         }
-//         else {
-//             setTimeout(function () { flippedCards[0].classList.remove('turn') }, 400)
-//             setTimeout(function () { flippedCards[1].classList.remove('turn') }, 400)
-//             setTimeout(function () { flippedCards.splice(0, 2) }, 400)
-//         }
-//     }
-//     let available = document.querySelectorAll('.available')
-//     available.forEach(flip => flip.style.pointerEvents = 'auto')
-//     let foundCards = document.querySelectorAll('.turned')
-//     if (foundCards.length === cards.length) {
-//         document.body.style.backgroundColor = 'darkgreen'
-//     }
-// }))
-
-
-// function newGame() {
-//     cards.forEach(card => card.classList.remove('turn', 'turned'))
-//     cards.forEach(card => {
-//         card.classList.remove('available')
-//         card.classList.toggle('available')
-//         card.style.pointerEvents = 'auto'
-//     })
-//     document.body.style.removeProperty('background-color')
-//     usedId = []
-//     usedOrder = []
-//     flippedCards = []
-//     apiImgs()
-
-// }
-
